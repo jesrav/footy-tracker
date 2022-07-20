d1806939995436333af55ba59d78fbffff12bec3
Jes Ravnbøl <jra@kapacity.dk>
2022-07-20 08:48:41 +0200

    WIP - splitting out backend into API

--- src/web/services/user_service.py
+++ src/web/services/user_service.py
@@ -4,38 +4,38 @@ from passlib.handlers.sha2_crypt import sha512_crypt as crypto
 import httpx
 from httpx import Response
 
+from models.user import User
 from models.validation_error import ValidationError
 
 BASE_WEB_API_URL = "http://127.0.0.1:8000"
 
 
-async def create_account(nickname: str, email: str, password: str):
+async def create_account(nickname: str, email: str, password: str) -> User:
     json_data = {"nickname": nickname, "password": password, "email": email}
     async with httpx.AsyncClient() as client:
         resp: Response = await client.post(url=BASE_WEB_API_URL + "/users/", json=json_data)
         if resp.status_code != 200:
             raise ValidationError(resp.text, status_code=resp.status_code)
-
-    return resp.json()
-
-
-# def login_user(email: str, password: str) -> Optional[User]:
-#     session = db_session.create_session()
-#
-#     try:
-#         user = session.query(User).filter(User.email == email).first()
-#         if not user:
-#             return user
-#
-#         if not crypto.verify(password, user.hash_password):
-#             return None
-#
-#         return user
-#     finally:
-#         session.close()
+    return User(**resp.json())
 
 
-async def get_user_by_id(user_id: int):
+
+async def login_user(email: str, password: str) -> Optional[User]:
+
+    try:
+        user = await get_user_by_email(email)
+        if not user:
+            return user
+
+        if not crypto.verify(password, user.hash_password):
+            return None
+
+        return user
+    finally:
+        session.close()
+
+
+async def get_user_by_id(user_id: int) -> User:
     async with httpx.AsyncClient() as client:
         resp: Response = await client.get(url=BASE_WEB_API_URL + f"/users/{user_id}")
         if resp.status_code == 404:
@@ -43,10 +43,10 @@ async def get_user_by_id(user_id: int):
         elif resp.status_code != 200:
             raise ValidationError(resp.text, status_code=resp.status_code)
         else:
-            return resp.json()
+            return User(**resp.json())
 
 
-async def get_user_by_email(email: str):
+async def get_user_by_email(email: str) -> User:
     async with httpx.AsyncClient() as client:
         resp: Response = await client.get(url=BASE_WEB_API_URL + f"/users/by_email/{email}")
         if resp.status_code == 404:
@@ -54,10 +54,10 @@ async def get_user_by_email(email: str):
         elif resp.status_code != 200:
             raise ValidationError(resp.text, status_code=resp.status_code)
         else:
-            return resp.json()
+            return User(**resp.json())
 
 
-async def get_user_by_nickname(nickname: str):
+async def get_user_by_nickname(nickname: str) -> User:
     async with httpx.AsyncClient() as client:
         resp: Response = await client.get(url=BASE_WEB_API_URL + f"/users/by_nickname/{nickname}")
         if resp.status_code == 404:
@@ -65,30 +65,14 @@ async def get_user_by_nickname(nickname: str):
         elif resp.status_code != 200:
             raise ValidationError(resp.text, status_code=resp.status_code)
         else:
-            return resp.json()
+            return User(**resp.json())
 
 
-# def get_user_by_email(email: str) -> Optional[User]:
-#     session = db_session.create_session()
-#
-#     try:
-#         return session.query(User).filter(User.email == email).first()
-#     finally:
-#         session.close()
-#
-#
-# def get_user_by_nickname(nickname: str) -> Optional[User]:
-#     session = db_session.create_session()
-#
-#     try:
-#         return session.query(User).filter(User.nickname == nickname).first()
-#     finally:
-#         session.close()
-#
-#
-# def get_all_users() -> List[User]:
-#     session = db_session.create_session()
-#     try:
-#         return session.query(User).all()
-#     finally:
-#         session.close()
+def get_all_users() -> List[User]:
+    user_limit = 1000
+    async with httpx.AsyncClient() as client:
+        resp: Response = await client.get(url=BASE_WEB_API_URL + f"/users/?limit{user_limit}")
+        if resp.status_code != 200:
+            raise ValidationError(resp.text, status_code=resp.status_code)
+        else:
+            return [User(**user_dict) for user_dict in resp.json()]
--- src/web/views/account.py
+++ src/web/views/account.py
@@ -42,43 +42,41 @@ async def register(request: Request):
 
     # Login user
     response = fastapi.responses.RedirectResponse(url='/account', status_code=status.HTTP_302_FOUND)
-    cookie_auth.set_auth(response, account["id"])
+    cookie_auth.set_auth(response, account.id)
     return response
 
 
-# ################### LOGIN #################################
-#
-# @router.get('/account/login')
-# @template(template_file='account/login.pt')
-# def login_get(request: Request):
-#     vm = LoginViewModel(request)
-#     return vm.to_dict()
-#
-#
-# @router.post('/account/login')
-# @template(template_file='account/login.pt')
-# async def login_post(request: Request):
-#
-#     vm = LoginViewModel(request)
-#     await vm.load()
-#
-#     if vm.error:
-#         return vm.to_dict()
-#
-#     user = user_service.login_user(vm.email, vm.password)
-#     if not user:
-#         vm.error = "The account does not exist or the password is wrong."
-#         return vm.to_dict()
-#
-#     resp = fastapi.responses.RedirectResponse('/account', status_code=status.HTTP_302_FOUND)
-#     cookie_auth.set_auth(resp, user.id)
-#
-#     return resp
-#
-#
-# @router.get('/account/logout')
-# def logout():
-#     response = fastapi.responses.RedirectResponse(url='/', status_code=status.HTTP_302_FOUND)
-#     cookie_auth.logout(response)
-#
-#     return response
+@router.get('/account/login')
+@template(template_file='account/login.pt')
+def login_get(request: Request):
+    vm = LoginViewModel(request)
+    return vm.to_dict()
+
+
+@router.post('/account/login')
+@template(template_file='account/login.pt')
+async def login_post(request: Request):
+
+    vm = LoginViewModel(request)
+    await vm.load()
+
+    if vm.error:
+        return vm.to_dict()
+
+    user = user_service.login_user(vm.email, vm.password)
+    if not user:
+        vm.error = "The account does not exist or the password is wrong."
+        return vm.to_dict()
+
+    resp = fastapi.responses.RedirectResponse('/account', status_code=status.HTTP_302_FOUND)
+    cookie_auth.set_auth(resp, user.id)
+
+    return resp
+
+
+@router.get('/account/logout')
+def logout():
+    response = fastapi.responses.RedirectResponse(url='/', status_code=status.HTTP_302_FOUND)
+    cookie_auth.logout(response)
+
+    return response
