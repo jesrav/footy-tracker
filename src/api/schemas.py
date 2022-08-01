a3a1dae93a2653684d9b4615e1a19f0ec7fc487b
jes <jesravnbol@hotmail.com>
2022-08-01 10:23:58 +0200

    WIP

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -48,6 +48,10 @@ class User(UserBase, table=True):
 
     ratings: List["UserRating"] = Relationship(back_populates="user")
 
+    @property
+    def latest_rating(self):
+        return sorted(self.ratings, key=lambda x: x.created_dt, reverse=True)[0]
+
 
 class UserCreate(UserBase):
     password: str
