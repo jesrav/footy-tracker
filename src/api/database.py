142608cb739389fd26d08fc4c8fa9f7366014500
Jes Ravnbøl <jra@kapacity.dk>
2022-08-04 08:22:52 +0200

    removed commented out code

--- src/api/database.py
+++ src/api/database.py
@@ -2,11 +2,7 @@ import os
 
 from sqlmodel import SQLModel, create_engine, Session
 
-#DATABASE_URL = "sqlite:///./footy_tracker.db"
 DATABASE_URL = os.environ.get("DATABASE_URL")
-#DATABASE_USER = os.environ.get("PG_USER")
-#DATABASE_PSW = os.environ.get("PG_PSW")
-#SQLALCHEMY_DATABASE_URL = "postgresql://footy:9Fockspace@postgresserver/footy"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
