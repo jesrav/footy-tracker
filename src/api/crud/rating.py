c3bf3a05c5852d035d3e47972ace7559be88fd4c
Jes Ravnbøl <jra@kapacity.dk>
2022-08-07 13:45:00 +0200

    wip

--- src/api/crud/rating.py
+++ src/api/crud/rating.py
@@ -6,11 +6,23 @@ from services.rating import get_updated_elo_player_ratings
 from models import rating as rating_models, result as result_models
 
 
-def add_rating(session: Session, user_id: int, rating_defence: float, rating_offence: float):
+from typing import List, Optional
+
+from sqlalchemy import func, and_
+from sqlmodel import Session, select
+
+from services.rating import get_updated_elo_player_ratings
+from models import rating as rating_models, result as result_models
+
+
+def add_rating(
+        session: Session, user_id: int, rating_defence: float, rating_offence: float, result_id: Optional[int] = None
+):
     user_rating = rating_models.UserRating(
         user_id=user_id,
         rating_defence=rating_defence,
         rating_offence=rating_offence,
+        latest_result_at_update_id=result_id,
     )
     session.add(user_rating)
     session.commit()
@@ -46,11 +58,36 @@ def get_latest_user_rating(session: Session, user_id: int) -> rating_models.User
     return session.exec(statement).first()
 
 
-def get_ratings(session: Session, user_id: int, skip: int = 0, limit: int = 100) -> List[rating_models.UserRating]:
+def get_user_ratings(
+        session: Session, user_id: int, skip: int = 0, limit: int = 100
+) -> List[rating_models.UserRating]:
     statement = (
         select(rating_models.UserRating)
         .filter(rating_models.UserRating.user_id == user_id)
         .offset(skip).limit(limit)
     )
     return session.exec(statement).all()
+
+
+def get_latest_ratings(session: Session) -> List[rating_models.UserRating]:
+    subquery = (
+        select(rating_models.UserRating.user_id, func.max(rating_models.UserRating.created_dt).label('maxdate'))
+        .group_by(rating_models.UserRating.user_id)
+        .subquery('t2')
+    )
+    statement = (
+        select(rating_models.UserRating)
+        .join(
+            subquery, and_(rating_models.UserRating.user_id == subquery.c.user_id,
+            rating_models.UserRating.created_dt == subquery.c.maxdate)
+        )
+    )
+    return session.exec(statement).all()
+
+
+
+
+
+
+
 
