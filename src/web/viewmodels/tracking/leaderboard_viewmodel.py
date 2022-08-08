b6205531a130510d9c535544c15e950ed0eb98c6
Jes Ravnbøl <jra@kapacity.dk>
2022-08-08 20:32:07 +0200

    Fixed ranking.

--- src/web/viewmodels/tracking/leaderboard_viewmodel.py
+++ src/web/viewmodels/tracking/leaderboard_viewmodel.py
@@ -19,5 +19,7 @@ class LeaderboardViewModel(ViewModelBase):
     async def load(self):
         self.user = await user_service.get_user_by_id(self.user_id)
         self.latest_user_ratings = await tracking_service.get_latest_user_ratings()
+        self.latest_user_ratings = sorted(self.latest_user_ratings, key=lambda x: x.rating, reverse=True)
+
         for i, user_rating in enumerate(self.latest_user_ratings, start=1):
             user_rating.ranking = i
