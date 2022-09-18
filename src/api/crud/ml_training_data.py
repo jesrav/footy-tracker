73a7aaef0d3e2ef9de1461d97a4c0f144b9cd6e0
Jes Ravnbøl <jra@kapacity.dk>
2022-09-18 12:29:00 +0200

    WIP

--- src/api/crud/ml_training_data.py
+++ src/api/crud/ml_training_data.py
@@ -20,7 +20,7 @@ ML_TRAINING_DATA_QUERY = """
       ) sub
       where result_id is not null
      )
-
+     
     select 
         rs.id as result_id,
         rs.team1_id,
@@ -31,30 +31,30 @@ ML_TRAINING_DATA_QUERY = """
         t1.attacker_user_id as team1_attacker_user_id,
         t2.defender_user_id as team2_defender_user_id,
         t2.attacker_user_id as team2_attacker_user_id,
-
+        
         rbr1d.overall_rating_before_game as team1_defender_overall_rating_before_game,
         rbr1d.defensive_rating_before_game as team1_defender_defensive_rating_before_game,
         rbr1d.offensive_rating_before_game as team1_defender_offensive_rating_before_game,
-
+        
         rbr1a.overall_rating_before_game as team1_attacker_overall_rating_before_game,
         rbr1a.defensive_rating_before_game as team1_attacker_defensive_rating_before_game,
         rbr1a.offensive_rating_before_game as team1_attacker_offensive_rating_before_game,
-
+        
         rbr2d.overall_rating_before_game as team2_defender_overall_rating_before_game,
         rbr2d.defensive_rating_before_game as team2_defender_defensive_rating_before_game,
         rbr2d.offensive_rating_before_game as team2_defender_offensive_rating_before_game,
-
+        
         rbr2a.overall_rating_before_game as team2_attacker_overall_rating_before_game,
         rbr2a.defensive_rating_before_game as team2_attacker_defensive_rating_before_game,
         rbr2a.offensive_rating_before_game as team2_attacker_offensive_rating_before_game
-
+        
     from resultsubmission rs
     inner join team t1 on rs.team1_id = t1.id
-    inner join team t2 on rs.team1_id = t2.id
-    inner join ratings_before_result rbr1d on rbr1d.user_id = t1.defender_user_id
-    inner join ratings_before_result rbr1a on rbr1a.user_id = t1.attacker_user_id
-    inner join ratings_before_result rbr2d on rbr2d.user_id = t2.defender_user_id
-    inner join ratings_before_result rbr2a on rbr2a.user_id = t2.attacker_user_id
+    inner join team t2 on rs.team2_id = t2.id
+    inner join ratings_before_result rbr1d on rbr1d.user_id = t1.defender_user_id and rs.id = rbr1d.result_id
+    inner join ratings_before_result rbr1a on rbr1a.user_id = t1.attacker_user_id and rs.id = rbr1a.result_id
+    inner join ratings_before_result rbr2d on rbr2d.user_id = t2.defender_user_id and rs.id = rbr2d.result_id
+    inner join ratings_before_result rbr2a on rbr2a.user_id = t2.attacker_user_id and rs.id = rbr2a.result_id
     where approved = true
     order by rs.id
 """
