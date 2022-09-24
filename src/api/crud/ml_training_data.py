3c801da408d8531f5e3ff34f7c25c2383b661b60
Jes Ravnbøl <jra@kapacity.dk>
2022-09-24 07:27:51 +0200

    training data wip

--- src/api/crud/ml_training_data.py
+++ src/api/crud/ml_training_data.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from numpy import random
 import pandas as pd
 from sqlmodel.ext.asyncio.session import AsyncSession
@@ -17,64 +19,69 @@ def add_ml_target(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-
-ML_TRAINING_DATA_QUERY = """
-    with ratings_before_result as (
-      select * from 
-      (
+def ml_data_query(n_rows: int) -> str:
+    if n_rows:
+        limit_statement = f"limit {n_rows}"
+    else:
+        limit_statement = ""
+    return f"""
+        with ratings_before_result as (
+          select * from 
+          (
+            select 
+                user_id, 
+                latest_result_at_update_id as result_id, 
+                lag(overall_rating, 1) over (order by user_id, created_dt) overall_rating_before_game,
+                lag(rating_defence, 1) over (order by user_id, created_dt) defensive_rating_before_game,
+                lag(rating_offence, 1) over (order by user_id, created_dt) offensive_rating_before_game
+            from userrating
+          ) sub
+          where result_id is not null
+         )
+         
         select 
-            user_id, 
-            latest_result_at_update_id as result_id, 
-            lag(overall_rating, 1) over (order by user_id, created_dt) overall_rating_before_game,
-            lag(rating_defence, 1) over (order by user_id, created_dt) defensive_rating_before_game,
-            lag(rating_offence, 1) over (order by user_id, created_dt) offensive_rating_before_game
-        from userrating
-      ) sub
-      where result_id is not null
-     )
-     
-    select 
-        rs.id as result_id,
-        rs.created_dt as result_dt,
-        rs.team1_id,
-        rs.team2_id,
-        rs.goals_team1,
-        rs.goals_team2,
-        t1.defender_user_id as team1_defender_user_id,
-        t1.attacker_user_id as team1_attacker_user_id,
-        t2.defender_user_id as team2_defender_user_id,
-        t2.attacker_user_id as team2_attacker_user_id,
-        
-        rbr1d.overall_rating_before_game as team1_defender_overall_rating_before_game,
-        rbr1d.defensive_rating_before_game as team1_defender_defensive_rating_before_game,
-        rbr1d.offensive_rating_before_game as team1_defender_offensive_rating_before_game,
-        
-        rbr1a.overall_rating_before_game as team1_attacker_overall_rating_before_game,
-        rbr1a.defensive_rating_before_game as team1_attacker_defensive_rating_before_game,
-        rbr1a.offensive_rating_before_game as team1_attacker_offensive_rating_before_game,
-        
-        rbr2d.overall_rating_before_game as team2_defender_overall_rating_before_game,
-        rbr2d.defensive_rating_before_game as team2_defender_defensive_rating_before_game,
-        rbr2d.offensive_rating_before_game as team2_defender_offensive_rating_before_game,
-        
-        rbr2a.overall_rating_before_game as team2_attacker_overall_rating_before_game,
-        rbr2a.defensive_rating_before_game as team2_attacker_defensive_rating_before_game,
-        rbr2a.offensive_rating_before_game as team2_attacker_offensive_rating_before_game
-        
-    from resultsubmission rs
-    inner join team t1 on rs.team1_id = t1.id
-    inner join team t2 on rs.team2_id = t2.id
-    inner join ratings_before_result rbr1d on rbr1d.user_id = t1.defender_user_id and rs.id = rbr1d.result_id
-    inner join ratings_before_result rbr1a on rbr1a.user_id = t1.attacker_user_id and rs.id = rbr1a.result_id
-    inner join ratings_before_result rbr2d on rbr2d.user_id = t2.defender_user_id and rs.id = rbr2d.result_id
-    inner join ratings_before_result rbr2a on rbr2a.user_id = t2.attacker_user_id and rs.id = rbr2a.result_id
-    where approved = true
-    order by rs.id
-"""
+            rs.id as result_id,
+            rs.created_dt as result_dt,
+            rs.team1_id,
+            rs.team2_id,
+            rs.goals_team1,
+            rs.goals_team2,
+            t1.defender_user_id as team1_defender_user_id,
+            t1.attacker_user_id as team1_attacker_user_id,
+            t2.defender_user_id as team2_defender_user_id,
+            t2.attacker_user_id as team2_attacker_user_id,
+            
+            rbr1d.overall_rating_before_game as team1_defender_overall_rating_before_game,
+            rbr1d.defensive_rating_before_game as team1_defender_defensive_rating_before_game,
+            rbr1d.offensive_rating_before_game as team1_defender_offensive_rating_before_game,
+            
+            rbr1a.overall_rating_before_game as team1_attacker_overall_rating_before_game,
+            rbr1a.defensive_rating_before_game as team1_attacker_defensive_rating_before_game,
+            rbr1a.offensive_rating_before_game as team1_attacker_offensive_rating_before_game,
+            
+            rbr2d.overall_rating_before_game as team2_defender_overall_rating_before_game,
+            rbr2d.defensive_rating_before_game as team2_defender_defensive_rating_before_game,
+            rbr2d.offensive_rating_before_game as team2_defender_offensive_rating_before_game,
+            
+            rbr2a.overall_rating_before_game as team2_attacker_overall_rating_before_game,
+            rbr2a.defensive_rating_before_game as team2_attacker_defensive_rating_before_game,
+            rbr2a.offensive_rating_before_game as team2_attacker_offensive_rating_before_game
+            
+        from resultsubmission rs
+        inner join team t1 on rs.team1_id = t1.id
+        inner join team t2 on rs.team2_id = t2.id
+        inner join ratings_before_result rbr1d on rbr1d.user_id = t1.defender_user_id and rs.id = rbr1d.result_id
+        inner join ratings_before_result rbr1a on rbr1a.user_id = t1.attacker_user_id and rs.id = rbr1a.result_id
+        inner join ratings_before_result rbr2d on rbr2d.user_id = t2.defender_user_id and rs.id = rbr2d.result_id
+        inner join ratings_before_result rbr2a on rbr2a.user_id = t2.attacker_user_id and rs.id = rbr2a.result_id
+        where approved = true
+        order by rs.id desc
+        {limit_statement}
+    """
 
 
-async def get_ml_training_data(session: AsyncSession) -> pd.DataFrame:
-    results = await session.execute(text(ML_TRAINING_DATA_QUERY))
+async def get_ml_data(session: AsyncSession, n_rows: Union[int, None] = None) -> pd.DataFrame:
+    results = await session.execute(text(ml_data_query(n_rows)))
     results = results.all()
     df = pd.DataFrame(
         columns=[
--- src/api/routes/ml.py
+++ src/api/routes/ml.py
@@ -4,17 +4,18 @@ from fastapi import Depends, APIRouter
 from sqlmodel.ext.asyncio.session import AsyncSession
 from starlette.responses import StreamingResponse
 
-from crud.ml_training_data import get_ml_training_data
+from crud.ml_training_data import get_ml_data
 from core.deps import get_session
 
 router = APIRouter()
 
 
-@router.get("/ml/data/", response_class=StreamingResponse, tags=["stats"])
+@router.get("/ml/training_data/csv", response_class=StreamingResponse, tags=["stats"])
 async def read_results(
-        session: AsyncSession = Depends(get_session)
+    nrows: int,
+    session: AsyncSession = Depends(get_session)
 ):
-    results_with_features_df = await get_ml_training_data(session)
+    results_with_features_df = await get_ml_data(session, nrows)
     stream = io.StringIO()
 
     results_with_features_df.to_csv(stream, index=False)
@@ -24,3 +25,12 @@ async def read_results(
     )
     response.headers["Content-Disposition"] = "attachment; filename=data.csv"
     return response
+
+
+@router.get("/ml/training_data/json", tags=["stats"])
+async def read_results(
+    nrows: int,
+    session: AsyncSession = Depends(get_session)
+):
+    results_with_features_df = await get_ml_data(session, nrows)
+    return results_with_features_df.to_dict()
