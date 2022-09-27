ec484eeb69696774411cdec081182e2668347b17
Jes Ravnbøl <jra@kapacity.dk>
2022-09-27 21:58:12 +0200

    ML background tasks: wip

--- src/ml_api/main.py
+++ src/ml_api/main.py
@@ -1,41 +1,65 @@
 """
-FastAPI app to host 5 stop prediction project
+FootyTracker ML microservice example
 """
-import os
+import math
 from random import choice
-from typing import List
 
-from fastapi import FastAPI, HTTPException, Depends, status
-import pandas as pd
+from fastapi import FastAPI
 
 from schemas import RowForML, DataForML
 
 app = FastAPI()
 
 
-def predict_goal_diff(result_to_predict: RowForML) -> int:
+def predict_goal_diff_rule(result_to_predict: RowForML) -> int:
     """"Predict the goal diff off the game using only the row of features for the actual game.
 
     If Jesus (user id 1) is on offence, his team will always win by 5
     Otherwise we predict a random goal diff between -3 and 3, but never 0.
     """
 
-    print(result_to_predict.team1_attacker_user_id)
     if result_to_predict.team1_attacker_user_id == 1:
         return 5
     elif result_to_predict.team2_attacker_user_id == 1:
         return -5
     else:
-        return 8 #choice([-3, -2, -1, 1, 2, 3])
+        return choice([-3, -2, -1, 1, 2, 3])
+
+
+def magic_footy_sigmoid(x):
+    """"Magic footy sigmoid that outputs value between -10 and 10"""
+    if x >= 0:
+        z = math.exp(-x)
+        return 20 * (1 / (1 + z)) - 10
+    else:
+        z = math.exp(x)
+        return 20 * (z / (1 + z)) - 10
+
+
+def predict_goal_diff_based_on_ratings(result_to_predict: RowForML) -> int:
+    """"
+    Naive rule to predict the result based on the teams combined rating
+    """
+    magic_factor = 100
+
+    team1_rating = (
+        result_to_predict.team1_attacker_offensive_rating_before_game
+        + result_to_predict.team1_defender_defensive_rating_before_game
+    ) / 2
+    team2_rating = (
+        result_to_predict.team2_attacker_offensive_rating_before_game
+        + result_to_predict.team2_defender_defensive_rating_before_game
+    ) / 2
+    return round(magic_footy_sigmoid((team1_rating - team2_rating) / magic_factor))
 
 
 @app.post("/rule_based_predict")
 def predict(body: DataForML) -> int:
     result_to_predict = [r for r in body.data if r.result_to_predict][0]
-    return predict_goal_diff(result_to_predict)
+    return predict_goal_diff_rule(result_to_predict)
 
 
-@app.post("/train")
-async def train() -> str:
-    return
-
+@app.post("/rating_based_predict")
+def predict(body: DataForML) -> int:
+    result_to_predict = [r for r in body.data if r.result_to_predict][0]
+    return predict_goal_diff_based_on_ratings(result_to_predict)
