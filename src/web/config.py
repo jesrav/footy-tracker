dd7e6097522974fcce50cabc2c6f28a7905d71b4
Jes Ravnbøl <jesravnbol@hotmail.com>
2022-10-10 09:01:09 +0200

    Moved API base url to config

--- src/web/config.py
+++ src/web/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     BLOB_STORAGE_BASE_URL: str
     BLOB_PROFILE_IMAGE_CONTAINER: str = "profileimages"
     BLOB_STORAGE_CON_STR: str
+    BASE_WEB_API_URL: str
 
     class Config:
         case_sensitive = True
