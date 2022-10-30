a52ec0077a4bb28154a623f7ff335585b75e7681
Jes Ravnbøl <jesravnbol@hotmail.com>
2022-10-30 16:22:41 +0100

    wip

--- src/ml_api/train_baysian.py
+++ src/ml_api/train_baysian.py
@@ -1,10 +1,11 @@
-import aesara.tensor as at
 import arviz as az
 import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pymc as pm
+from sklearn.base import BaseEstimator, TransformerMixin
+
 
 RANDOM_SEED = 8927
 rng = np.random.default_rng(RANDOM_SEED)
@@ -23,65 +24,96 @@ def magic_footy_sigmoid(x):
 
 df = pd.read_csv('footy-data.csv')
 
-users = list(set(
-    df['team1_defender_user_id'].tolist()
-    + df['team1_attacker_user_id'].tolist()
-    + df['team2_defender_user_id'].tolist()
-    + df['team2_attacker_user_id'].tolist()
-))
+#users = list(set(
+#    df['team1_defender_user_id'].tolist()
+#    + df['team1_attacker_user_id'].tolist()
+#    + df['team2_defender_user_id'].tolist()
+#    + df['team2_attacker_user_id'].tolist()
+#))
 
-team1_def_idx = df.team1_defender_user_id.apply(lambda x: users.index(x))
-team1_att_idx = df.team1_attacker_user_id.apply(lambda x: users.index(x))
-team2_def_idx = df.team2_defender_user_id.apply(lambda x: users.index(x))
-team2_att_idx = df.team2_attacker_user_id.apply(lambda x: users.index(x))
+#team1_def_idx = df.team1_defender_user_id.apply(lambda x: users.index(x))
+#team1_att_idx = df.team1_attacker_user_id.apply(lambda x: users.index(x))
+#team2_def_idx = df.team2_defender_user_id.apply(lambda x: users.index(x))
+#team2_att_idx = df.team2_attacker_user_id.apply(lambda x: users.index(x))
 
-footy_model = pm.Model()
 
-with pm.Model(coords={"user": users}) as model:
-    # constant data
-    team1_def = pm.ConstantData("team1_def", team1_def_idx, dims="match")
-    team1_att = pm.ConstantData("team1_att", team1_att_idx, dims="match")
-    team2_def = pm.ConstantData("team2_def", team2_def_idx, dims="match")
-    team2_att = pm.ConstantData("team2_att", team2_att_idx, dims="match")
+class FootyModel(BaseEstimator, TransformerMixin):
+    def __init__(self, mc_sample_size=1500, mc_tune_size=1500):
+        self.model = pm.Model()
+        self.users = None
+        self.trace = None
+        self.attack_strengths = None
+        self.defensive_strengths = None
+        self.mc_sample_size = mc_sample_size
+        self.mc_tune_size = mc_tune_size
 
-    # global model parameters
-    sd_att = pm.HalfNormal("sd_att", sigma=2)
-    sd_def = pm.HalfNormal("sd_def", sigma=2)
-    intercept = pm.Normal("intercept", mu=3, sigma=1)
-    sd = pm.HalfNormal("sd", sigma=1)
+    def fit(self, df):
+        self.users = list(set(
+            df['team1_defender_user_id'].tolist()
+            + df['team1_attacker_user_id'].tolist()
+            + df['team2_defender_user_id'].tolist()
+            + df['team2_attacker_user_id'].tolist()
+        ))
+        team1_def_idx = df.team1_defender_user_id.apply(lambda x: self.users.index(x))
+        team1_att_idx = df.team1_attacker_user_id.apply(lambda x: self.users.index(x))
+        team2_def_idx = df.team2_defender_user_id.apply(lambda x: self.users.index(x))
+        team2_att_idx = df.team2_attacker_user_id.apply(lambda x: self.users.index(x))
 
-    # team-specific model parameters
-    atts = pm.Normal("atts_star", mu=0, sigma=sd_att, dims="user")
-    defs = pm.Normal("defs_star", mu=0, sigma=sd_def, dims="user")
+        with pm.Model(coords={"user": self.users}) as model:
+            # constant data
+            team1_def = pm.ConstantData("team1_def", team1_def_idx, dims="match")
+            team1_att = pm.ConstantData("team1_att", team1_att_idx, dims="match")
+            team2_def = pm.ConstantData("team2_def", team2_def_idx, dims="match")
+            team2_att = pm.ConstantData("team2_att", team2_att_idx, dims="match")
 
-    team1_theta = at.exp(intercept + atts[team1_att_idx] + defs[team1_def])
-    team2_theta = at.exp(intercept + atts[team2_att_idx] + defs[team2_def_idx])
+            # global model parameters
+            sd_att = pm.HalfNormal("sd_att", sigma=2)
+            sd_def = pm.HalfNormal("sd_def", sigma=2)
+            sd = pm.HalfNormal("sd", sigma=1)
 
-    # likelihood of observed data
-    goal_diff = pm.Normal(
-        "goal_diff",
-        mu=team1_theta - team2_theta,
-        sigma=sd,
-        observed=df["goal_diff"],
-        dims=("match"),
-    )
-    trace = pm.sample(1000, tune=1000, cores=1)
+            # User-specific model parameters
+            attack_strength = pm.Normal("attack_strength", mu=0, sigma=sd_att, dims="user")
+            defensive_strength = pm.Normal("defensive_strength", mu=0, sigma=sd_def, dims="user")
+
+            team1_theta = attack_strength[team1_att_idx] + defensive_strength[team1_def_idx]
+            team2_theta = attack_strength[team2_att_idx] + defensive_strength[team2_def_idx]
+
+            # likelihood of observed data
+            goal_diff = pm.Normal(
+                "goal_diff",
+                mu=team1_theta - team2_theta,
+                sigma=sd,
+                observed=df["goal_diff"],
+                dims="match",
+            )
+            self.trace = pm.sample(self.mc_sample_size, tune=1500, cores=1)
+
+        attack_strengths_values = self.trace.posterior["attack_strength"].mean(dim=("chain", "draw")).values
+        self.attack_strengths = {u: attack_strengths_values[i] for i, u in enumerate(self.users)}
+
+        defensive_strengths_values = self.trace.posterior["defensive_strength"].mean(
+            dim=("chain", "draw")).values
+        self.defensive_strengths = {u: defensive_strengths_values[i] for i, u in enumerate(self.users)}
+
+
+
+footy_model = FootyModel()
+footy_model.fit(df)
 
 
 ##########################################################################
 # Analysis
 ##########################################################################
-az.summary(trace, kind="diagnostics")
+az.summary(footy_model.trace, kind="diagnostics")
 
-trace_hdi = az.hdi(trace)
+trace_hdi = az.hdi(footy_model.trace)
 
 _, ax = plt.subplots(figsize=(12, 6))
-
-ax.scatter(users, trace.posterior["atts_star"].median(dim=("chain", "draw")), color="C0", alpha=1, s=100)
+ax.scatter(footy_model.users, footy_model.trace.posterior["attack_strength"].median(dim=("chain", "draw")), color="C0", alpha=1, s=100)
 ax.vlines(
-    users,
-    trace_hdi["atts_star"].sel({"hdi": "lower"}),
-    trace_hdi["atts_star"].sel({"hdi": "higher"}),
+    footy_model.users,
+    trace_hdi["attack_strength"].sel({"hdi": "lower"}),
+    trace_hdi["attack_strength"].sel({"hdi": "higher"}),
     alpha=0.6,
     lw=5,
     color="C0",
@@ -91,13 +123,13 @@ ax.set_ylabel("Posterior Attack Strength")
 ax.set_title("HDI of user Attack Strength");
 plt.show()
 
-_, ax = plt.subplots(figsize=(12, 6))
 
-ax.scatter(users, trace.posterior["defs_star"].median(dim=("chain", "draw")), color="C0", alpha=1, s=100)
+_, ax = plt.subplots(figsize=(12, 6))
+ax.scatter(footy_model.users, footy_model.trace.posterior["defensive_strength"].median(dim=("chain", "draw")), color="C0", alpha=1, s=100)
 ax.vlines(
-    users,
-    trace_hdi["defs_star"].sel({"hdi": "lower"}),
-    trace_hdi["defs_star"].sel({"hdi": "higher"}),
+    footy_model.users,
+    trace_hdi["defensive_strength"].sel({"hdi": "lower"}),
+    trace_hdi["defensive_strength"].sel({"hdi": "higher"}),
     alpha=0.6,
     lw=5,
     color="C0",
