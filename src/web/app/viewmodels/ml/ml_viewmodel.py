5e6db97142dd2d2aa8b33e80680991b28208eb18
Jes Ravnbøl <jesravnbol@hotmail.com>
2022-11-06 09:26:27 +0100

    wip

--- src/web/app/viewmodels/ml/ml_viewmodel.py
+++ src/web/app/viewmodels/ml/ml_viewmodel.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 from starlette.requests import Request
 
@@ -12,9 +12,16 @@ class MLViewModel(ViewModelBase):
         super().__init__(request)
         self.user_ml_models: List[MLModel] = []
         self.ml_models: List[MLModelRead] = []
-        self.ml_metrics: List[MLMetric] = []
+        self.model_ml_metrics: Dict[int, List[MLMetric]] = {}
+        self.model_latest_ml_metric: Dict[int, MLMetric] = {}
 
     async def load(self):
         self.user_ml_models = await ml_service.get_user_ml_models(self.bearer_token)
         self.ml_models = await ml_service.get_ml_models()
-        self.ml_metrics = await ml_service.get_ml_metrics()
+        ml_metrics = await ml_service.get_ml_metrics()
+        for ml_model_id in [m.id for m in self.ml_models]:
+            self.model_ml_metrics[ml_model_id] = [metric for metric in ml_metrics if metric.ml_model_id == ml_model_id]
+        for model_id in self.model_ml_metrics:
+            self.model_latest_ml_metric[model_id] = sorted(
+                self.model_ml_metrics[model_id], key=lambda x: x.prediction_dt
+            )[-1]
