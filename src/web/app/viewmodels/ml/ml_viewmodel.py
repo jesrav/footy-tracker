9140ccf635757b17bd6be33f8f9d654bcf7568b5
Jes Ravnbøl <jesravnbol@hotmail.com>
2022-11-06 13:27:02 +0100

    wip

--- src/web/app/viewmodels/ml/ml_viewmodel.py
+++ src/web/app/viewmodels/ml/ml_viewmodel.py
@@ -10,13 +10,15 @@ from app.viewmodels.shared.viewmodel import ViewModelBase
 class MLViewModel(ViewModelBase):
     def __init__(self, request: Request):
         super().__init__(request)
-        self.user_ml_models: List[MLModel] = []
+        self.user_ml_models: Dict[int, MLModel] = {}
         self.ml_models: List[MLModelRead] = []
         self.model_ml_metrics: Dict[int, List[MLMetric]] = {}
         self.model_latest_ml_metric: Dict[int, MLMetric] = {}
+        self.model_css_ids = ["model-1", "model-2", "model-3"]
 
     async def load(self):
-        self.user_ml_models = await ml_service.get_user_ml_models(self.bearer_token)
+        user_ml_models = await ml_service.get_user_ml_models(self.bearer_token)
+        self.user_ml_models = {i: ml_model for i, ml_model in enumerate(user_ml_models)}
         self.ml_models = await ml_service.get_ml_models()
         ml_metrics = await ml_service.get_ml_metrics()
         for ml_model_id in [m.id for m in self.ml_models]:
